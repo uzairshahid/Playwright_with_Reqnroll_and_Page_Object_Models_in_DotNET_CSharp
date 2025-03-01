8ec4e1ec425320300baf961a4068d8f9223b7fac
Uzair Shahid <uzairshahid7111@gmail.com>
2025-03-01 01:36:46 +0000

    rebuild

--- Playwrights_Web_Automation/obj/Debug/net8.0/Playwrights_Web_Automation.AssemblyInfo.cs
+++ Playwrights_Web_Automation/obj/Debug/net8.0/Playwrights_Web_Automation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Playwrights_Web_Automation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb304fa71f8376cb7824a35b27fede860aebc5a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84a4e4b6774da96a47c945e221a57d8d7b11bf8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Playwrights_Web_Automation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Playwrights_Web_Automation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
